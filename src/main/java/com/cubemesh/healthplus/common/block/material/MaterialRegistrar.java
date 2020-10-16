8d848c065d06e75990242627b1859aaeb375bbee
Areeb Mohammed <areebmohammed11@gmail.com>
2020-10-15 20:12:19 -0500

    Miscellaneous helper files for the future

---
+++ src/main/java/com/cubemesh/healthplus/common/block/material/MaterialRegistrar.java
@@ -0,0 +1,8 @@
+package com.cubemesh.healthplus.common.block.material;
+
+import net.minecraft.block.material.Material;
+import net.minecraft.block.material.MaterialColor;
+
+public class MaterialRegistrar {
+    public static final Material GRIME = (new Material.Builder(MaterialColor.AIR)).build();
+}
