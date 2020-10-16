9aa20cb159d00d0d9ff5b91cd5dfc291d82a2ae6
Areeb Mohammed <areebmohammed11@gmail.com>
2020-10-15 20:10:49 -0500

    Added random ticks to Grime block

---
+++ src/main/java/com/cubemesh/healthplus/common/block/BlockRegistrar.java
@@ -0,0 +1,48 @@
+package com.cubemesh.healthplus.common.block;
+
+import com.cubemesh.healthplus.HealthPlus;
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemModelsProperties;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class BlockRegistrar {
+
+    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, HealthPlus.MODID);
+    public static final DeferredRegister<Item> BLOCKITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, HealthPlus.MODID);
+
+    public static void init()
+    {
+        BLOCKS.register(FMLJavaModLoadingContext.get().getModEventBus());
+        BLOCKITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
+    }
+
+    /*
+            Block Registration
+     */
+    public static final RegistryObject<Block> GRIME = BLOCKS.register("grime", () ->
+            new Grime(
+                AbstractBlock.Properties.create(Material.EARTH)
+                        .doesNotBlockMovement()
+                        .speedFactor(0.5f)
+                        .hardnessAndResistance(0.5f, 0.1f)
+                        .tickRandomly()
+            )
+    );
+
+    /*
+            BlockItem Registration
+     */
+    public static final RegistryObject<Item> GRIME_ITEM = BLOCKITEMS.register("grime", () ->
+            new BlockItem(
+                GRIME.get(), new Item.Properties().group(ItemGroup.DECORATIONS)
+            )
+    );
+}
